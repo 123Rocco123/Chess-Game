df78b2d460f058ea27276d0709310b7ccbf76e7a
123Rocco123 <petruccio.rocco@gmail.com>
2021-01-13 22:18:56 +0100

    Knights Movement
    This commit adds the functionaility of the knights movement, both in the vertical and horizontal planes.
    
    It also fixes some bugs in the index dictionary used in the class.

--- Chess.py
+++ Chess.py
@@ -95,14 +95,14 @@ class pieceMovement:
         possible_positions = ["1", "2", "3", "4", "5", "6", "7", "8"]
         pieces = ["R", "H", "B", "K", "Q"]
 
-        rows = {"1" : [0,1,2,3,4,5,6,7,8],
-                "2" : [9,10,11,12,13,14,15,16],
-                "3" : [17,18,19,20,21,22,23,24],
-                "4" : [25,26,27,28,29,30,31,32],
-                "5" : [33,34,35,36,37,38,39,40],
-                "6" : [41,42,43,44,45,46,47,48],
-                "7" : [49,50,51,52,53,54,55,56],
-                "8" : [57,58,59,60,61,62,63,64]
+        rows = {"1" : [0,1,2,3,4,5,6,7],
+                "2" : [8,9,10,11,12,13,14,15],
+                "3" : [16,17,18,19,20,21,22,23],
+                "4" : [24,25,26,27,28,29,30,31],
+                "5" : [32,33,34,35,36,37,38,39],
+                "6" : [40,41,42,43,44,45,46,47],
+                "7" : [48,49,50,51,52,53,54,55],
+                "8" : [56,57,58,59,60,61,62,63]
                 }
 
         desired_move = input("Where do you want to move the piece to? ")
@@ -171,12 +171,30 @@ class pieceMovement:
                         piece = "K" + team + numbers
                         #The if statement below is used to determine how the vertical movements of the knight, both in the positive (down) and negative (up) directions. 
                         if desired_move == (chess_board0[piece + 17] and chess_board0[piece * numbers] == "X") or \
-                                           (chess_board0[piece + 15] and chess_board0[piece * numbers] == "X") or \
-                                           (chess_board0[piece - 17] and chess_board0[piece * numbers] == "X") or \
-                                           (chess_board0[piece - 15] and chess_board0[piece * numbers] == "X"):
+                           desired_move == (chess_board0[piece + 15] and chess_board0[piece * numbers] == "X") or \
+                           desired_move == (chess_board0[piece - 17] and chess_board0[piece * numbers] == "X") or \
+                           desired_move == (chess_board0[piece - 15] and chess_board0[piece * numbers] == "X"):
                             switch
                         
-                        #elif desired_move == 
+                        #This else if statement is used to determine which positions are possible for the knight to move in the horizontal axis of the board.
+                        elif desired_move == (chess_board0[piece + 10] and chess_board0[piece * numbers] == "X") or \
+                             desired_move == (chess_board0[piece + 6] and chess_board0[piece * numbers] == "X") or \
+                             desired_move == (chess_board0[piece - 10] and chess_board0[piece * numbers] == "X") or \
+                             desired_move ==  (chess_board0[piece - 6] and chess_board0[piece * numbers] == "X"):
+                            for row in rows:
+                                for indexes in rows[row]:
+                                    row_of_start = chess_board0[piece]
+                                    row_of_end = chess_board0[desired_move]
+                                    
+                                    if indexes == row_of_start:
+                                        initial_row = row
+                                    if indexes == row_of_end:
+                                        final_row = row
+                            if initial_row != final_row:
+                                switch
+                            else:
+                                print("Invalid movement.")
+                                self.rules()
 
     def move(self):
         selected_piece = input("What piece do you want to move?")
